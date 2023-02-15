99b9f18ca11e52dd2e8a667810748d8f40d04df8
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2023-02-15 12:29:49 +0530

    adding transport url

--- src/grpc.options.ts
+++ src/grpc.options.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 export const microserviceOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
+    url: 'localhost:50051',
     package: 'app',
     protoPath: join(__dirname, '../src/app.proto'),
   },
